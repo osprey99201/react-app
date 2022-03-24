b7ba8a64f00d18ec2b37a9c774fe60449f48bc39
Osprey99201 <saadbinosama@gmail.com>
2022-03-24 22:35:37 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+// import react from 'react';
+import Header from './components/header'
+
+function App() {
+ 
+  return (
+    <div className='container'>
+     <Header title= 'saad'/>
+    </div>
+
+  );
+}
+
+// class App extends react.Component {
+//   render() {
+//     return <h1>hello from class</h1>
+//   }
+// }
+export default App;
